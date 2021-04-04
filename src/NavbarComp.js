281cba2fbfac752fb81f0bc30777eeaa35970b58
Aaftab Pinjari <pinjari.aaftab@gmail.com>
2021-04-04 15:41:02 +0530

    done with nav-bar

--- src/NavbarComp.js
+++ src/NavbarComp.js
@@ -1,39 +1,44 @@
 import React, { useState, useRef, useEffect } from 'react'
 import Navbar from 'react-bootstrap/Navbar'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { BsJustify } from 'react-icons/bs'
+import { FaBars } from 'react-icons/fa'
 import { links, social } from './data'
 import logo from './logo.svg'
 
 const NavbarComp = () => {
+
+  const [showLinks,setShowLinks]=useState(true)
+  const linksContainerRef= useRef(null)
+  const linksRef=useRef(null)
+
+
   return (
     <nav>
       <div className="nav-center">
         <div className="'nav-header">
-          <img src={logo} alt='logo' />
-          <button classsName='nav-toggle'>
-            <BsJustify />
+          <h2 className="logo">Aaftab Pinjari</h2>
+          <button classsName='nav-toggle'
+          onClick={()=> setShowLinks(!showLinks)}>
+            <FaBars />
           </button>
         </div>
-        <div className='links-container show-container'>
-          <ul className="links">
-            <li>
-              <a href='#'>Home</a>
-            </li>
-            <li>
-              <a href='#'>About</a>
-            </li>
-            <li>
-              <a href='#'>Contact</a>
-            </li>
-            <li>
-              <a href='#'>Projects</a>
-            </li>
+        {showLinks && <div className="links-container show-container" 
+        ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+            {links.map((link)=>{
+              const {id,url,text}=link;
+              return <li key={id} >
+                <a href={url}>{text}</a>
+              </li>
+            })}
           </ul>
-        </div>
+        </div>}
         <ul className="social-icons">
-
-        
+            {social.map((link)=>{
+              const {id,url,icon}=link;
+              return <li key={id} >
+                <a href={url}>{icon}</a>
+              </li>
+            })}
         </ul>
       
       
